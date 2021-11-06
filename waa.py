20bbadefc8284aeccad8031f0eb462b15f1af110
DavidNovikov <dn9678@gmail.com>
2021-11-06 15:41:15 -0400

    data cleaned

--- waa.py
+++ waa.py
@@ -12,8 +12,10 @@ plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
 columns = ["Latitude", "Longitude", "CrashSeverity"]
 df = pd.read_csv("crashclean2.csv", usecols=columns)
+df = df.drop(df[df.Latitude > 90].index)
+df = df.drop(df[df.Longitude < -90].index)
 colors = [accidentTypeToColor[crashtype] for crashtype in df.CrashSeverity]
 plt.gca().legend(accidentTypeToColor)
 print("Contents in csv file:\n", df)
-plt.scatter(df.Latitude, df.Longitude, c=colors)
+plt.scatter(df.Latitude, df.Longitude, c=colors, s=0.5)
 plt.show()
